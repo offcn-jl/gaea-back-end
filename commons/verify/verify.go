032c199d1d25ba5506313681e2b5ee569db5b2c7
冯国清 <master@rebeta.cn>
2020-12-03 15:15:09 +0800

    feat: 增加校验 Mis 口令码的工具;

--- commons/verify/verify.go
+++ commons/verify/verify.go
@@ -9,7 +9,18 @@
 
 package verify
 
-import "regexp"
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"errors"
+	"fmt"
+	"github.com/offcn-jl/gaea-back-end/commons/config"
+	"github.com/offcn-jl/gaea-back-end/commons/logger"
+	"github.com/offcn-jl/gaea-back-end/commons/request"
+	"regexp"
+	"strconv"
+	"time"
+)
 
 // Phone 用来验证手机号码是否有效
 // 中国(严谨), 根据工信部2019年最新公布的手机号段
@@ -18,4 +29,34 @@ import "regexp"
 func Phone(phone string) bool {
 	regular := `^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-7|9])|(?:5[0-3|5-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[1|8|9]))\d{8}$`
 	return regexp.MustCompile(regular).MatchString(phone)
+}
+
+// MisToken 校验 MIS 口令码 是否合法
+func MisToken(misToken string) (bool, error) {
+	// 获取当前系统中的 MIS 口令码
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	if responseJsonMap, err := request.GetSendQueryReceiveJson(config.Get().OffcnMisURL, map[string]string{"appid": config.Get().OffcnMisAppID, "sign": fmt.Sprintf("%x", sha1.Sum([]byte("appid="+config.Get().OffcnMisAppID+"&code="+fmt.Sprintf("%x", md5.Sum([]byte(config.Get().OffcnMisCode)))+"&noncestr=gaea&timestamp="+timestamp+"&token="+config.Get().OffcnMisToken+"&url=http://gaea.jilinoffcn.com/"))), "noncestr": "gaea", "timestamp": timestamp}); err != nil {
+		// 请求失败
+		return false, err
+	} else {
+		// 判断是否获取成功
+		if responseJsonMap["status"].(float64) != 1 {
+			logger.DebugToJson("响应内容", responseJsonMap)
+			// 获取 MIS TOKEN 失败
+			if responseJsonMap["msg"] != nil {
+				return false, errors.New(responseJsonMap["msg"].(string))
+			} else {
+				return false, errors.New("请求 MIS 口令码 失败")
+			}
+		} else {
+			// 判断 MIS TOKEN 是否有效
+			if misToken != responseJsonMap["data"] {
+				// MIS Token 不匹配
+				return false, nil
+			} else {
+				// 对 UUID 对应的 Session 进行更新 MIS Token 的操作
+				return true, nil
+			}
+		}
+	}
 }
--- commons/verify/verify_test.go
+++ commons/verify/verify_test.go
@@ -10,11 +10,14 @@
 package verify
 
 import (
+	"github.com/jarcoal/httpmock"
+	"github.com/offcn-jl/gaea-back-end/commons/response"
 	. "github.com/smartystreets/goconvey/convey"
+	"net/http"
 	"testing"
 )
 
-// 测试 Phone 函数是否可以用来验证手机号码是否有效
+// TestPhone 测试 Phone 函数是否可以用来验证手机号码是否有效
 func TestPhone(t *testing.T) {
 	Convey("测试 Phone 函数是否可以用来验证手机号码是否有效", t, func() {
 		So(Phone("17866668888"), ShouldBeTrue)      // 178 号段
@@ -24,5 +27,47 @@ func TestPhone(t *testing.T) {
 		So(Phone("27866668888"), ShouldBeFalse)     // 287 号段
 		So(Phone("+0117866668888"), ShouldBeFalse)  // 带国家码 +01
 		So(Phone("000117866668888"), ShouldBeFalse) // 带国家码 0001
+	})
+}
+
+// TestMisToken 测试 MisToken 是否可以校验 MIS 口令码 是否合法
+func TestMisToken(t *testing.T) {
+	Convey("测试 MisToken 是否可以校验 MIS 口令码 是否合法", t, func() {
+		// 测试请求失败的情况
+		// 此时未配置接口地址, 会直接返回请求出错
+		pass, err := MisToken("")
+		So(pass, ShouldBeFalse)
+		So(err.Error(), ShouldContainSubstring, "unsupported protocol scheme \"\"")
+
+		// 配置 httpmock 进行拦截
+		httpmock.Activate()
+		defer httpmock.DeactivateAndReset()
+		httpmock.RegisterNoResponder(httpmock.NewJsonResponderOrPanic(http.StatusOK, response.Struct{"status": 2, "msg": "时间戳缺失或失效"}))
+
+		// 测试 获取 MIS TOKEN 失败 并返回了错误信息
+		pass, err = MisToken("")
+		So(pass, ShouldBeFalse)
+		So(err.Error(), ShouldContainSubstring, "时间戳缺失或失效")
+
+		// 修改 httpmock 为不返回错误信息
+		httpmock.RegisterNoResponder(httpmock.NewJsonResponderOrPanic(http.StatusOK, response.Struct{"status": 2}))
+
+		// 测试 获取 MIS TOKEN 失败 并没有返回错误信息
+		pass, err = MisToken("")
+		So(pass, ShouldBeFalse)
+		So(err.Error(), ShouldContainSubstring, "请求 MIS 口令码 失败")
+
+		// 修改 httpmock 为返回成功
+		httpmock.RegisterNoResponder(httpmock.NewJsonResponderOrPanic(http.StatusOK, response.Struct{"status": 1, "msg": "SIGN验签成功", "data": "fake-token"}))
+
+		// 测试 MIS Token 错误
+		pass, err = MisToken("wrong-fake-token")
+		So(pass, ShouldBeFalse)
+		So(err, ShouldBeNil)
+
+		// 测试 MIS Token 正确
+		pass, err = MisToken("fake-token")
+		So(pass, ShouldBeTrue)
+		So(err, ShouldBeNil)
 	})
 }
